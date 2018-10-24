3e5de6fd94fd2b50f662a0ed4a2b273c00947357
Nancy Agrawal <nagraw5@uic.edu>
2018-10-24 18:23:49 -0500

    src/FirstUniqueCharacter.java

--- LeetCode/src/FirstUniqueCharacter.java
+++ LeetCode/src/FirstUniqueCharacter.java
@@ -6,7 +6,9 @@ public class FirstUniqueCharacter {
 
 	public static void main(String[] args) {
 		String s = "nancyagrawal";
-		System.out.println(firstUniqCharUsingHashMap(s));
+		System.out.println(firstUniqCharWithHashMapAndHashSet(s));
+		System.out.println(firstUniqCharWithHashMap(s));
+		
 		
 	}
 	
@@ -16,7 +18,7 @@ public class FirstUniqueCharacter {
 	 * @param s
 	 * @return
 	 */
-	public static int firstUniqCharUsingHashMap(String s) {
+	public static int firstUniqCharWithHashMapAndHashSet(String s) {
 
         HashSet<Integer> indicesSet = new HashSet<Integer>(s.length());
         for(int i=0; i<s.length(); i++){
@@ -35,6 +37,32 @@ public class FirstUniqueCharacter {
         if(indicesSet.isEmpty())
             return -1;
         return Collections.min(indicesSet);
+    }
+	
+	
+	/**
+	 * Using only HashMap
+	 * Time Complexity would still be O(n) but practically faster.
+	 * @param s
+	 * @return
+	 */
+	public static int firstUniqCharWithHashMap(String s) {
+        if(s.isEmpty())
+            return -1;
+        
+        HashMap<Character, Integer> lettersMap = new HashMap<Character, Integer>();
+        for(int i=0; i<s.length(); i++){
+            Character c = s.charAt(i);
+            if(lettersMap.containsKey(c))
+                lettersMap.put(c, s.length());
+            else 
+                lettersMap.put(c, i);            
+        }
+        
+        int minIndex = Collections.min(lettersMap.values());
+        if(minIndex == s.length())
+            return -1;
+        return minIndex;
     }
 	
 }
